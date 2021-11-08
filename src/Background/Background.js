8dc57edd0f63cbdf1fab45ced81b0df203566cc7
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-09 01:43:53 +0200

    Add background module and use composes to leverage styles

--- src/Background/Background.js
+++ src/Background/Background.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import styles from './Background.module.css'
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Background</h1>
+        <div className={styles.stars}></div>
+        <div className={styles.alpha}></div>
+        <div className={styles.nebula}></div>
       </div>
     )
   }
