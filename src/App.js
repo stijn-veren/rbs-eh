77def836e51b919a1d73827ca3fa4f65f91aadf7
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-09 02:06:10 +0200

    Refactor App component into stateful and add state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import styles from './App.module.css'
+import { Component } from 'react'
+
 import Title from './Title/Title'
 import Background from './Background/Background'
 import Grid from './Grid/Grid'
 import Rocket from './Rocket/Rocket'
 import Smoke from './Rocket/Smoke'
 
-function App() {
-  return (
-    <div className={styles.App}>
-      <Title />
-      <Background />
-      <Grid />
-      <Rocket />
-      <Smoke />
-    </div>
-  )
+// CSS
+import styles from './App.module.css'
+
+// Data
+import facts from './data/NASA_facts.json'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      facts,
+    }
+  }
+
+  render() {
+    return (
+      <div className={styles.App}>
+        <Title />
+        <Background />
+        <Grid facts={this.state.facts} />
+        <Rocket />
+        <Smoke />
+      </div>
+    )
+  }
 }
 
 export default App
