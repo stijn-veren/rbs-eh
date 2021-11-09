4c27fc142101e2f9902d90e70451fc9e1270f54d
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-09 02:06:42 +0200

    Complete Grid component and module

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
+import GridItem from './GridItem'
+
+// CSS
+import styles from './Grid.module.css'
+
 export default class App extends React.Component {
+  renderFacts() {
+    return this.props.facts.map((item) => (
+      <GridItem key={item.id} fact={item} />
+    ))
+  }
+
   render() {
-    return (
-      <div>
-        <h1>Grid</h1>
-      </div>
-    )
+    return <section className={styles.grid}>{this.renderFacts()}</section>
   }
 }
