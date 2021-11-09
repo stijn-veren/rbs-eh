c7ea35c6ef286fd119ee3e57590e66ff5bbd72a6
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-09 02:06:58 +0200

    Complete Grid Item component and module

--- src/Grid/GridItem.js
+++ src/Grid/GridItem.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import styles from './GridItem.module.css'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Grid Item</h1>
-      </div>
-    )
-  }
+const GridItem = (props) => {
+  return (
+    <div className={styles.card}>
+      <h2 className={styles.title}>{props.fact.title}</h2>
+      <article className={styles.fact}>{props.fact.fact}</article>
+    </div>
+  )
 }
+
+export default GridItem
